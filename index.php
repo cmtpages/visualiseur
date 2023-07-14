5da9f8638e3a35d9c326b2d333a26dca1e9aa62b
clement <contact@clementpages.fr>
2023-07-14 11:03:44 +0200

    Design de index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="fr">
 <head>
 	<title>Caméra document</title>
+	
+	<link rel="stylesheet" href="css/design.css">
+	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 	<script src="qrcode.min.js"></script>
 </head>
@@ -11,21 +14,22 @@
 	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 	header("Cache-Control: no-cache");
 	header("Pragma: no-cache");
+
+	$id=uniqid("", false); // Génère d'id de la session et le nom de la photo
 ?>
-<?php $id=uniqid("", false); ?>
 <h1>Accueil</h1>
 <h2>Prise de vue (avec un portable)</h2>
 <a href="photo.php?id=<?php echo $id; ?>">Prise de vue</a>
-<div id="qrcode"></div>
+<p>Scanner le QRcode avec votre téléphone pour pouvoir prendre des photos.</p>
+<div class="cadre" id="qrcode"></div>
 <script>
 	console.log(window.location.origin);
 	var id = <?php echo json_encode($id, JSON_HEX_TAG); ?>;
 	new QRCode(document.getElementById("qrcode"), window.location.origin+"/visualiseur/photo.php?id="+id);
 </script>
 
-<h2>Ordi</h2>
-<a href="view.php?id=<?php echo $id; ?>">Projeter l'image</a>
-
+<h2>Projection (avec un ordinateur)</h2>
+<p class="cadre">Sur cet ordinateur, cliquez ici : <a href="view.php?id=<?php echo $id; ?>">projeter l'image</a></p>
 
 </body>
 </html>
