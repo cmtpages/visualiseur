4ae6a3a57b881650300f0f44b2c50b860d3ac9d3
clement <contact@clementpages.fr>
2023-07-15 10:17:56 +0200

    Amélioration du bouton refresh, meilleure gestion des nouvelles photos

--- view.php
+++ view.php
@@ -35,8 +35,11 @@ Copyright 2023 Clément PAGÈS
 		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 		header("Cache-Control: no-cache");
 		header("Pragma: no-cache");
+		
+		session_start();
+		
+// 		print_r($_SESSION);
 	?>
-
 	<?php $id = $_GET['id']; ?>
 	<menu>
 		<li><a href="view.php?id=<?php echo $id; ?>">
@@ -44,21 +47,60 @@ Copyright 2023 Clément PAGÈS
 		</a></li>
 		<li><img class="button_view" src="asset/img/rotate2.png" alt="Tourner l'image" onclick="rotate90()" /></li>
 	</menu>
-
-	<?php if(!file_exists('uploads/'.$id.'.png')) { ?>
-		<p>Aucune photo pour le moment.</p>
-	<?php }
-	else {
-		$img_path = 'uploads/'.$id.'.png'; 
-		
-		// Copie l'image dans une variable pour effacer celle enregistrée en dur
-		// sans perdre l'affichage.
-		$contents = file_get_contents($img_path);
-		$base64 = base64_encode($contents);
-		unlink($img_path); 	?>
+	<?php if(!isset($_SESSION['id'])) {
+		if(!file_exists('uploads/'.$id.'.png')) { ?>
+			<p>Aucune photo pour le moment.</p>
+		<?php }
+		else {
+			$_SESSION['id'] = $id;
+			$img_path = 'uploads/'.$id.'.png'; 
+			// Copie l'image dans une variable pour effacer celle enregistrée en dur
+			// sans perdre l'affichage.
+			$contents = file_get_contents($img_path);
+			$base64 = base64_encode($contents);
+			$_SESSION['base54'] = $base64;
 			
-		<img id="photo_actuelle" src="data:image/jpg;base64,<?php echo $base64; ?>">'
-	<?php } ?>
+			unlink($img_path); ?>
+				
+			<img id="photo_actuelle" src="data:image/jpg;base64,<?php echo $_SESSION['base54']; ?>">'
+		<?php }
+	}
+	elseif(isset($_SESSION['id']) and $_SESSION['id'] != $id) { // Nouvelle session
+		if(!file_exists('uploads/'.$id.'.png')) { ?>
+			<p>Aucune photo pour le moment.</p>
+		<?php }
+		else {
+			$_SESSION['id'] = $id;
+			$img_path = 'uploads/'.$id.'.png'; 
+			// Copie l'image dans une variable pour effacer celle enregistrée en dur
+			// sans perdre l'affichage.
+			$contents = file_get_contents($img_path);
+			$base64 = base64_encode($contents);
+			$_SESSION['base54'] = $base64;
+			
+			unlink($img_path); ?>
+				
+			<img id="photo_actuelle" src="data:image/jpg;base64,<?php echo $_SESSION['base54']; ?>">'
+		<?php }
+	}
+	else { // Même session
+		if(!file_exists('uploads/'.$id.'.png')) { ?> <!-- Ancienne image -->
+			<img id="photo_actuelle" src="data:image/jpg;base64,<?php echo $_SESSION['base54']; ?>"
+		<?php }
+		else { // Nouvelle image
+			$img_path = 'uploads/'.$id.'.png'; 
+			// Copie l'image dans une variable pour effacer celle enregistrée en dur
+			// sans perdre l'affichage.
+			$contents = file_get_contents($img_path);
+			$base64 = base64_encode($contents);
+			$_SESSION['base54'] = $base64;
+			
+			unlink($img_path); ?>
+				
+			<img id="photo_actuelle" src="data:image/jpg;base64,<?php echo $_SESSION['base54']; ?>">'
+		<?php } 
+	} ?>
+
 </body>
 </html>
 
