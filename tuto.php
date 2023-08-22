00a6aa850da239d044dade42fd58064518a4da15
clement <contact@clementpages.fr>
2023-08-22 12:47:19 +0200

    Mise à jour du manuel utilisateur

--- tuto.php
+++ tuto.php
@@ -40,17 +40,17 @@ Licence  CC BY-NC-SA 2.0 FR https://creativecommons.org/licenses/by-nc-sa/2.0/fr
 	</dl>
 
 	<figure>
-		<img class="img_border" src="asset/img/tuto_accueil.png" alt="Capture d'écran de la page d'accueil" height="500"/>
 		<figcaption>Capture d'écran de la page d'accueil</figcaption>
+		<img class="img_border" src="asset/img/tuto_accueil.png" alt="Capture d'écran de la page d'accueil" height="500"/>
 	</figure>
 
 	<h3>Prise de photo avec le smartphone</h3>
 	<p class="align-left">Une fois le QR-code scanné, il faut ouvrir la page, puis autoriser l'utilisation de l'appareil photo. On voit ce que filme la caméra. On appuie sur <img  class="img_border" src="asset/img/photo.png" alt="Télécharger" width="24"/> pour prendre une photo. Puis le bouton <img class="img_border" src="asset/img/upload.png" alt="Prendre en photo" width="24"/> envoie l'image sur le serveur afin de la partager avec l'ordinateur.</p>
 
 	<figure>
-	<img class="img_border" src="asset/img/tuto_autorisation.jpg" alt="Capture d'écran de la demande d'autorisation pour accéder à l'appareil photo depuis un smartphone" height="500" />
-	<img class="img_border" src="asset/img/tuto_priseDeVue.jpg" alt="Capture d'écran de la page permettant la prise de photos depuis un smartphone" height="500" />
-	<figcaption>Captures d'écran : autorisation d'accès à la caméra et prise de photo</figcaption>
+		<figcaption>Captures d'écran : autorisation d'accès à la caméra et prise de photo</figcaption>
+		<img class="img_border" src="asset/img/tuto_autorisation.jpg" alt="Capture d'écran de la demande d'autorisation pour accéder à l'appareil photo depuis un smartphone" height="500" />
+		<img class="img_border" src="asset/img/tuto_priseDeVue.jpg" alt="Capture d'écran de la page permettant la prise de photos depuis un smartphone" height="500" />
 	</figure>
 
 	<p class="align-left">Il est à noter que les photos sont supprimées immédiatement après leur envoi et ne sont pas conservées. Lorsque la photo a été envoyée, il est possible d'en prendre une autre et de la télécharger à nouveau. La photo précédente est alors remplacée par la nouvelle.</p>
@@ -60,8 +60,9 @@ Licence  CC BY-NC-SA 2.0 FR https://creativecommons.org/licenses/by-nc-sa/2.0/fr
 
 	<p class="align-left">Il y a simplement deux boutons. En cliquant sur <img src="asset/img/refresh.png" alt="Actualiser" width="24"/>, on actualise la page pour charger une éventuelle nouvelle photo. Le bouton <img src="asset/img/rotate2.png" alt="Actualiser" width="24"/> tourne l'image d'un quart de tour.</p>
 
-	<figure><img src="asset/img/tuto_projection.png" alt="Capture d'écran de la page de projection" height="500" />
-	<figcaption>Capture d'écran : page de projection depuis l'ordinateur</figcaption>
+	<figure>
+		<figcaption>Capture d'écran : page de projection depuis l'ordinateur</figcaption>
+		<img src="asset/img/tuto_projection.png" alt="Capture d'écran de la page de projection" height="500" />
 	</figure>
 </div>
 
